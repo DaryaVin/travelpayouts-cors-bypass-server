665eebb1db83ff3cb70e60c85218b57a4736f8c3
DaryaVin <31697627+DaryaVin@users.noreply.github.com>
2024-04-03 14:02:20 +0700

    change port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import fetch from "node-fetch";
 // const cors = require('cors');
 // const fetch = require('node-fetch');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
