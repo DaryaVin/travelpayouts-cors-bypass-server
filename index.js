a4b74ec2465faa4003605b872bc1337ec0d0d321
daryavin1995 <31697627+DaryaVin@users.noreply.github.com>
2024-09-12 01:23:22 +0700

    add server

--- index.js
+++ index.js
@@ -2,58 +2,133 @@ import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
 
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const fetch = require('node-fetch');
-
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const app = express();
 
 app.use(cors());
 const corsOptions = {
-    origin: "http://localhost:3000"
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(express.json());
+const HOST = "https://test.v5.pryaniky.com";
 
-
-const token = "43b0dbc155c5db417b74b3b60707333b";
-
-
-
-app.post('/getAviasales', cors(corsOptions), async (req, res) => {
-    let currentRequtstEndPoint = "https://api.travelpayouts.com/aviasales/v3/prices_for_dates?";
-
-    for (const key in req.body) {
-        currentRequtstEndPoint += key + "=" + req.body[key] + "&"
-    }
-    currentRequtstEndPoint += "token=" + token;
+// login
+app.post(
+  "/ru/data/v3/testmethods/docs/login",
+  cors(corsOptions),
+  async (req, res) => {
+    const currentRequtstEndPoint = HOST + "/ru/data/v3/testmethods/docs/login";
+    console.log("currentRequtstEndPoint", currentRequtstEndPoint);
 
     const fetchOptions = {
-        method: 'GET' 
-    }
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(req.body),
+    };
+
     const response = await fetch(currentRequtstEndPoint, fetchOptions);
+
     const jsonResponse = await response.json();
     res.json(jsonResponse);
-});
-app.post('/bookingAirTicker', cors(corsOptions), async (req, res) => {
-    res.json({...req.body, success: true});
-});
+  }
+);
 
-app.get('/getData/:type', cors(corsOptions), async (req, res) => {
-    let currentRequtstEndPoint = "http://api.travelpayouts.com/data/ru/" + req.params.type + ".json";
-    console.log("currentRequtstEndsPoint", currentRequtstEndPoint);
+// readAll
+app.get(
+  "/ru/data/v3/testmethods/docs/userdocs/get",
+  cors(corsOptions),
+  async (req, res) => {
+    const currentRequtstEndPoint =
+      HOST + "/ru/data/v3/testmethods/docs/userdocs/get";
 
     const fetchOptions = {
-        method: 'GET' 
-    }
+      method: "GET",
+      headers: { "x-auth": req.headers["x-auth"] },
+    };
+
     const response = await fetch(currentRequtstEndPoint, fetchOptions);
+
     const jsonResponse = await response.json();
     res.json(jsonResponse);
-});
+  }
+);
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+// create
+app.post(
+  "/ru/data/v3/testmethods/docs/userdocs/create",
+  cors(corsOptions),
+  async (req, res) => {
+    const currentRequtstEndPoint =
+      HOST + "/ru/data/v3/testmethods/docs/userdocs/create";
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "x-auth": req.headers["x-auth"],
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(req.body),
+    };
+
+    const response = await fetch(currentRequtstEndPoint, fetchOptions);
+
+    const jsonResponse = await response.json();
+    res.json(jsonResponse);
+  }
+);
+// update
+app.post(
+  "/ru/data/v3/testmethods/docs/userdocs/set/:id",
+  cors(corsOptions),
+  async (req, res) => {
+    const currentRequtstEndPoint =
+      HOST + "/ru/data/v3/testmethods/docs/userdocs/set/" + req.params.id;
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "x-auth": req.headers["x-auth"],
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(req.body),
+    };
+
+    const response = await fetch(currentRequtstEndPoint, fetchOptions);
+
+    const jsonResponse = await response.json();
+    res.json(jsonResponse);
+  }
+);
+// delete
+app.post(
+  "/ru/data/v3/testmethods/docs/userdocs/delete/:id",
+  cors(corsOptions),
+  async (req, res) => {
+    const currentRequtstEndPoint =
+      HOST + "/ru/data/v3/testmethods/docs/userdocs/delete/" + req.params.id;
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "x-auth": req.headers["x-auth"],
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(req.body),
+    };
+
+    const response = await fetch(currentRequtstEndPoint, fetchOptions);
+
+    const jsonResponse = await response.json();
+    res.json(jsonResponse);
+  }
+);
+
+app.listen(PORT, async () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
